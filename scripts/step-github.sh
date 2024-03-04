3998b4263c8cc4e03f48644946744ed2125936a2
Viacheslav Borisov <slavb18@iconicompany.com>
2024-03-04 08:01:45 +0400

    update

--- scripts/step-github.sh
+++ scripts/step-github.sh
@@ -45,6 +45,8 @@ user:
     client-key-data: ${PRIVATEKEY_BASE64}
 EOF
 
+cat $HOME/.kube/config
+
 echo 'KUBE_CONFIG<<EOF' >> $GITHUB_OUTPUT
 cat $HOME/.kube/config | base64 >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
